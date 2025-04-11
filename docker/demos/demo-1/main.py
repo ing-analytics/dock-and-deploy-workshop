222009ae46c4df32c4fe9b871fe844017ecd28d6
Andra-Maria Acsintoae <andra-maria.acsintoae@ing.com>
2025-04-11 13:46:30 +0200

    add docker demo and exercise for part 1

---
+++ docker/demos/demo-1/main.py
@@ -0,0 +1,78 @@
+import logging
+import os
+import uvicorn
+
+from fastapi import FastAPI, Request, Body
+from fastapi.logger import logger
+from contextlib import asynccontextmanager
+from sql import CREATE_USER_TABLE, INSERT_USER, SELECT_ONE, SELECT_USER
+
+app = FastAPI()
+logger.setLevel(logging.DEBUG)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("HOORAY! APP STARTED")
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+# Hello World Demo
+@app.get("/hello")
+async def index():
+   logger.info("HELLO REQUEST")
+   return {"message": "Hello World"}
+
+
+@app.get("/healthz")
+async def health():
+   return {"Status": "Healthy"}
+
+
+# With Database
+@app.get("/db-health")
+async def db_health(request: Request):
+   try:
+      conn = request.app.state.db
+      cursor = conn.cursor()
+      cursor.execute(SELECT_ONE)
+   except Exception:
+      {"Status": "DB is unhealthy..."}
+   return {"Status": "DB is healthy! Yey"}
+
+
+@app.get("/current-user")
+async def get_curr_user(request: Request):
+   try:
+      conn = request.app.state.db
+      cursor = conn.cursor()
+      cursor.execute(SELECT_USER)
+      row = cursor.fetchone()
+   except Exception as e:
+      return {"Error": e}
+   return {"Current user": row[0]}
+
+
+@app.post("/current-user")
+async def update_curr_user(request: Request, payload: dict = Body(...)):
+   try:
+      conn = request.app.state.db
+      cursor = conn.cursor()
+   except Exception as e:
+      return {"Error": e}
+   
+   try:
+      cursor.execute(CREATE_USER_TABLE)
+   except Exception:
+      logger.info("TABLE IS ALREADY CREATED")
+      pass
+
+      cursor.execute(INSERT_USER.format(user=payload["username"]))
+      logger.info("New current user is set")
+   return {"Status": "New Current User Set"}
+
+if __name__ == "__main__":
+   host = os.getenv("APP_HOST", "0.0.0.0")
+   port = os.getenv("APP_PORT", "8000")
+
+   uvicorn.run(app, host=host, port=int(port))
